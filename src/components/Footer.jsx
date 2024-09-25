18ec265f3454462de261a48393255cd6abe893c1
Igoro2016 <igorcabrobo@hotmail.com>
2024-09-25 11:13:05 -0300

    .

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import './Footer.css'
 export default function Footer() {
   return (
     <footer className='footer'>
-        <h2>Desenvolvido por Elaine Tavares</h2>
+        <h2>Desenvolvido por Igor Gomes</h2>
         <img src="/logo_borboleta.png" alt="" />
     </footer>
   )
